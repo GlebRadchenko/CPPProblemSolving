ed8eabca7b6f9da13c8f7d4456dc4f54229a000c
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2017-01-22 23:03:56 +0200

    Initial commit

---
+++ Anton/SortAlgorithms/SortAlgorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SortAlgorithms
+//
+//  Created by Anton Poltoratskyi on 22.01.17.
+//  Copyright © 2017 Anton Poltoratskyi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
