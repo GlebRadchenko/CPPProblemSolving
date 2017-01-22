368badfb6f7744769568072d9e91bad6668de680
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2017-01-22 23:32:38 +0200

    Implemented bubble sort

--- Anton/SortAlgorithms/SortAlgorithms/main.cpp
+++ Anton/SortAlgorithms/SortAlgorithms/main.cpp
@@ -7,8 +7,68 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+#pragma mark - Bubble Sort
+
+void bubbleSort(vector<int> &array) {
+    
+    for (int i = 0; i < array.size(); i++) {
+        for (int j = i + 1; j < array.size(); j++) {
+            
+            if (array[j] < array[i]) {
+                swap(array[i], array[j]);
+            }
+        }
+    }
+}
+
+
+#pragma mark - Test Data
+
+vector<int>testArray() {
+    
+    vector<int> array;
+    
+    int maxValue = 100;
+    int size = 10;
+    
+    for (int i = 0; i < size; i++) {
+        array.push_back(rand() % maxValue + 1);
+    }
+    return array;
+}
+
+
+#pragma mark - Utils
+
+void swap(int &obj1, int &obj2) {
+    int temp = obj1;
+    obj1 = obj2;
+    obj2 = temp;
+}
+
+void print(vector<int> array) {
+    for (int i = 0; i < array.size(); i++) {
+        cout << array[i];
+        cout << " ";
+    }
+    cout << "\n";
+}
+
+
+#pragma mark - Main
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+    
+    vector<int> array = testArray();
+    print(array);
+    
+    bubbleSort(array);
+    print(array);
+    
     return 0;
 }
